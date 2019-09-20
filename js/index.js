45bd733535db2c6115953cd0dae217f1609a3661
Wisam Naji <guitar90rocker@gmail.com>
2019-09-20 16:37:52 +0300

    added edit mode

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
+let form = document.querySelector('#todo-input');
+form.onsubmit = submitEventHandler;
+
+let todoTable = document.querySelector(".todo-table");
+
+let todoArray = [];
+let editIndex = -1;
+
 function submitEventHandler(event) {
     event.preventDefault();
     let input = document.querySelector("#todo-input input");
     let todoText = input.value;
     input.value = "";
-    let todo = createTodo(todoText);
-    todoArray.push(todo);
+    if (editIndex < 0) {
+        let todo = createTodo(todoText);
+        todoArray.push(todo);
+    } else {
+        let todo = todoArray[editIndex];
+        todo.text = todoText;
+        editIndex = -1;
+    }
     updateTable();
 }
 
-function updateTable(){
+function updateTable() {
     todoTable.innerHTML = '<div class="table-row table-head"><div class="table-cell grow">Todo Description</div><div class="table-cell">Added On</div><div class="table-cell">Actions</div></div>';
-    for(let i=0; i < todoArray.length; i++) {
+    for (let i = 0; i < todoArray.length; i++) {
         let todo = todoArray[i];
         let HTMLTemplate = `<div class="table-cell grow">${todo.text}</div><div class="table-cell">${todo.time}</div><div class="table-cell"><a class="todo-delete" href="#">Delete</a><a class="todo-edit" href="#">Edit</a></div>`;
         let wrapper = document.createElement("div");
@@ -18,18 +32,16 @@ function updateTable(){
         wrapper.innerHTML = HTMLTemplate;
         wrapper.id = "todo-" + i;
         wrapper.querySelector('.todo-delete').addEventListener("click", onDeleteTodo);
+        wrapper.querySelector('.todo-edit').addEventListener("click", onEditTodo);
         todoTable.appendChild(wrapper);
     }
 };
 
-let form = document.querySelector('#todo-input');
-form.onsubmit = submitEventHandler;
-
-let todoTable = document.querySelector(".todo-table");
-
-let todoArray = [];
 function createTodo(todoText) {
-    return {text: todoText, time: "Sep 12, 03:12 PM"};
+    return {
+        text: todoText,
+        time: "Sep 12, 03:12 PM"
+    };
 }
 
 function onDeleteTodo(e) {
@@ -40,3 +52,11 @@ function onDeleteTodo(e) {
     updateTable();
 }
 
+function onEditTodo(e) {
+    e.preventDefault();
+    let todoWrapper = e.target.parentNode.parentNode;
+    let todoIndex = parseInt(todoWrapper.id.split('-')[1]);
+    let todo = todoArray[todoIndex];
+    document.querySelector("#todo-input input").value = todo.text;
+    editIndex = todoIndex;
+}
